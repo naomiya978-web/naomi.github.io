64d60bca84eed502ece9a1d3ad5ef35875fb8d40
Naomi <naomiya978@gmail.com>
2026-06-28 04:08:52 +0800

    Update main-script.js

--- main-script.js
+++ main-script.js
@@ -15,7 +15,7 @@ if (canvas) {
     resizeCanvas();
 
     const fishImage = new Image();
-    fishImage.src = 'images/fish.PNG';
+    fishImage.src = 'images/fish.png';
 
     const FISH_COUNT = 10;
     const FISH_SIZE = 80;
